e52a253408d05418ae331d6bc193be63da86dbf7
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-05-05 10:14:15 -0500

    added to seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (1, 'Classic Burger', 1, 09/19/2008);
+
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (2, 'Mushroom Swiss Burger', 2, 09/20/2008);
+
+INSERT INTO burgers (id, burger_name, devoured, date) VALUES (3, 'Turkey Burger', 3, 09/21/2008);
